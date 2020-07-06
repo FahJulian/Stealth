b298b067b88afcb0714dbdee559b77a8d4a01435
Julian Fahrländer <julian@pop-os.localdomain>
2020-07-06 19:44:28 +0200

    Fixed shader bug

--- src/main/resources/shaders/batched_textured_rectangle.glsl
+++ src/main/resources/shaders/batched_textured_rectangle.glsl
@@ -32,5 +32,56 @@ uniform sampler2D uTextures[16];
 void main()
 {
     int textureSlot = int(fTextureSlot);
-    color = texture(uTextures[textureSlot], fTextureCoords);
+
+    switch (textureSlot)
+    {
+        case 0:
+            color = texture(uTextures[0], fTextureCoords);
+            return;
+        case 1:
+            color = texture(uTextures[1], fTextureCoords);
+            return;
+        case 2:
+            color = texture(uTextures[2], fTextureCoords);
+            return;
+        case 3:
+            color = texture(uTextures[3], fTextureCoords);
+            return;
+        case 4:
+            color = texture(uTextures[4], fTextureCoords);
+            return;
+        case 5:
+            color = texture(uTextures[5], fTextureCoords);
+            return;
+        case 6:
+            color = texture(uTextures[6], fTextureCoords);
+            return;
+        case 7:
+            color = texture(uTextures[7], fTextureCoords);
+            return;
+        case 8:
+            color = texture(uTextures[8], fTextureCoords);
+            return;
+        case 9:
+            color = texture(uTextures[9], fTextureCoords);
+            return;
+        case 10:
+            color = texture(uTextures[10], fTextureCoords);
+            return;
+        case 11:
+            color = texture(uTextures[11], fTextureCoords);
+            return;
+        case 12:
+            color = texture(uTextures[12], fTextureCoords);
+            return;
+        case 13:
+            color = texture(uTextures[13], fTextureCoords);
+            return;
+        case 14:
+            color = texture(uTextures[14], fTextureCoords);
+            return;
+        case 15:
+            color = texture(uTextures[15], fTextureCoords);
+            return;
+    }
 }
